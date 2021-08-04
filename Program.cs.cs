0ae9fdf23b8103673b9cbf550165c86019bd5e03
tiagorodriguesbr <77688731+tiagorodriguesbr@users.noreply.github.com>
2021-08-04 18:22:32 -0300

    primeiro commit

---
+++ Program.cs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace scae
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDevezes = 5;
+
+            for (int i=0; i< numeroDevezes; i++)
+            {
+                Console.WriteLine($"Bem vindo a Scae! {i}");
+            }
+        }
+    }
+}
